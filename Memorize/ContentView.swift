8bdb1400ba5036000658ad1db4eeada6b190da8e
martapanc <marta.panc@gmail.com>
2024-08-10 16:54:17 +0200

    Playing around with Swift

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    // body is a _computed_property_
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        // some = the type of body needs to be any struct as long as it behaves like a View - aka 'some view'
+        VStack(alignment: .center, spacing: 30) { // Embedded function
+            Image(systemName: "tortoise")
+                .foregroundColor(Color.green)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Marta!")
+                .foregroundColor(Color.blue)
+                .font(.largeTitle)
+            Text("Just felt like adding another line here :)")
+                .foregroundColor(Color.teal)
         }
-        .padding()
+        .padding(40)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 4)
     }
 }
+
+
+
+
+
 
 #Preview {
     ContentView()
